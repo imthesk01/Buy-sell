c794f8da3bda6cde81753d6ac23b8e3957cafaf0
imthesk01 <64018806+imthesk01@users.noreply.github.com>
2020-12-30 20:18:20 +0530

    Update my-listings-page.component.ts

--- src/app/my-listings-page/my-listings-page.component.ts
+++ src/app/my-listings-page/my-listings-page.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-//import { fakeMyListings } from '../fake-data';
-import { ListingsService } from '../listings.service'
+import { fakeMyListings } from '../fake-data';
 import { Listing } from '../types';
 
 @Component({
@@ -11,72 +10,13 @@ import { Listing } from '../types';
 export class MyListingsPageComponent implements OnInit {
   listings: Listing[] = [];
 
-  constructor(
-    private listingsService: ListingsService,
-  ) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.listingsService.getListingsForUser()
-      .subscribe(listings => this.listings = listings);
+    this.listings = fakeMyListings;
   }
 
   onDeleteClicked(listingId: string): void {
-    this.listingsService.deleteListing(listingId)
-      .subscribe(() => {
-        this.listings = this.listings.filter(
-          listing => listing.id !== listingId
-        );
-      });
+    alert(`Deleting your listing with id ${listingId}`);
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-// @Component({
-//   selector: 'app-my-listings-page',
-//   templateUrl: './my-listings-page.component.html',
-//   styleUrls: ['./my-listings-page.component.css']
-// })
-
-// export class MyListingsPageComponent implements OnInit {
-//   listings: Listing[] = [];
-
-//   constructor(
-//     private listingsService: ListingsService,
-//     private router: Router) { }
-
-//   ngOnInit(): void {
-//     this.getListings();
-//   }
-
-//   getListings():void{
-//     this.listingsService.getListings()
-//     .subscribe(resp => {
-//       console.log(resp);
-//       this.listings=resp;
-//     })
-//   }
-
-
-//   onDeleteClicked(listingId: string): void {
-//     this.listingsService.deleteListing(listingId)
-//     .subscribe(data => {
-//       console.log(data);
-//       this.gethome();
-//     })
-//     //alert(`Deleting your listing with id ${listingId}`);
-//   }
-
-//   gethome(){
-//     this.router.navigate(['/']);
-//   }
-
-// }
