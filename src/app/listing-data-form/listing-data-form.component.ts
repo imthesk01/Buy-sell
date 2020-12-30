3943ff2915edc9064ec626858789612c1a089c0f
imthesk01 <64018806+imthesk01@users.noreply.github.com>
2020-12-30 20:20:04 +0530

    Update listing-data-form.component.ts

--- src/app/listing-data-form/listing-data-form.component.ts
+++ src/app/listing-data-form/listing-data-form.component.ts
@@ -35,7 +35,6 @@ export class ListingDataFormComponent implements OnInit {
       name: this.name,
       description: this.description,
       price: Number(this.price),
-     // views: 0,
     });
   }
 }
